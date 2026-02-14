5977d4afe6fe93c0be88819919d8a923b0944b3a
Vipul-045 <vipulyadav0479@gmail.com>
2026-02-14 14:51:59 +0530

    next js config for redirection

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+  return [
+    {
+      source: '/:shortcode',
+      destination: 'https://keepitshort.onrender.com/:shortcode',
+    },
+  ];
+}
 };
+
+
 
 export default nextConfig;
