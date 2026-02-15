505b6ba117db69fddcae47f2034eae7797ce47c4
Vipul-045 <vipulyadav0479@gmail.com>
2026-02-15 16:08:48 +0530

    db

--- src/services/db.rs
+++ src/services/db.rs
@@ -1,14 +1,14 @@
-use mongodb::{
-    Client, Collection, bson::de::Error, options::ClientOptions, results::{InsertOneResult}
-};
-use std::{env};
 use mongodb::bson::doc;
+use mongodb::{
+    Client, Collection, bson::de::Error, options::ClientOptions, results::InsertOneResult,
+};
+use std::env;
 
 use crate::models::url_model::ShortURL;
 
 #[derive(Clone)]
 pub struct DataStructure {
-        pub og_url : Collection<ShortURL>,
+    pub og_url: Collection<ShortURL>,
 }
 
 impl DataStructure {
@@ -19,12 +19,15 @@ impl DataStructure {
 
         let client = Client::with_options(client_options)?;
 
-        client.database("admin").run_command(doc! {"ping":1}).await?;
+        client
+            .database("admin")
+            .run_command(doc! {"ping":1})
+            .await?;
         println!("MongoDB Connected Successfully!");
 
         let collection = client.database("SHORT_URL").collection::<ShortURL>("urls");
 
-        Ok(Self{og_url: collection})
+        Ok(Self { og_url: collection })
     }
 
     pub async fn url_storage(&self, og_url: ShortURL) -> Result<InsertOneResult, Error> {
@@ -32,13 +35,16 @@ impl DataStructure {
             .og_url
             .insert_one(og_url)
             .await
-            .ok()
             .expect("Error inserting a url");
         Ok(result)
     }
 
-    pub async fn get_ogurl(&self, short_url: &str) -> Result<Option<ShortURL>, Error>{
-        let result = self.og_url.find_one(doc! {"short_url": short_url }).await.ok().expect("No url found");
+    pub async fn get_ogurl(&self, short_url: &str) -> Result<Option<ShortURL>, Error> {
+        let result = self
+            .og_url
+            .find_one(doc! {"short_url": short_url })
+            .await
+            .expect("No url found");
         Ok(result)
     }
 }
